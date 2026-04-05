759cffb3a5ac0a761d6420b698d2480bcc099682
Franknascimento <146045480+Franknascimento@users.noreply.github.com>
2026-04-05 14:04:37 -0400

    Add project documentation for ESP32 with DHT22

---
+++ README.m
@@ -0,0 +1,17 @@
+# Projeto ESP32 com Sensor DHT22 e LEDs
+
+Este projeto foi desenvolvido utilizando o simulador *Wokwi* para demonstrar o funcionamento de um sensor de temperatura e umidade (DHT22) conectado a um ESP32.
+
+## 🚀 Como visualizar a simulação
+Você pode ver o circuito funcionando em tempo real através deste link:
+[Clique aqui para abrir a simulação no Wokwi](https://wokwi.com/projects/458766984993707009)
+
+## 🛠️ Componentes utilizados:
+* ESP32 DevKit V4
+* Sensor de Temperatura e Umidade DHT22
+* 2 LEDs (Azul e Vermelho)
+* Resistores
+
+## 📄 Arquivos do Projeto
+* diagram.json: Configuração das conexões do circuito.
+* esboco.ino: Código fonte da lógica do sensor
